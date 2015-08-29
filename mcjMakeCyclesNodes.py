09b4f96b18e4fc6a446d5a9bebe52d9ebaf67f69
Mystic Gems <mystic.gems.sl@gmail.com>
2015-08-28 22:13:48 -0400

    Fix viewport specularity

--- mcjMakeCyclesNodes.py
+++ mcjMakeCyclesNodes.py
@@ -284,7 +284,7 @@ def fixObject( o, Glossfactor, GlossRough ):
 		mat = m.material
 		if mat:
 			fixMat( mat, Glossfactor, GlossRough, mat.name )
-            mat.specular_hardness = 50
+			mat.specular_hardness = 50
 		
 #---------- fixObjects ----------
 def fixObjects():
